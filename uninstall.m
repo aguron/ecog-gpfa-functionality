553784c40b998231082ae3f4306bf68004a3edce
aguron <akinomigbodun@yahoo.com>
2017-08-14 12:15:03 -0700

    Files added

---
+++ uninstall.m
@@ -0,0 +1,7 @@
+rmpath(pwd)
+rmpath([pwd, '/core_gpfa'])
+rmpath([pwd, '/core_twostage'])
+rmpath([pwd, '/plotting'])
+rmpath([pwd, '/util'])
+rmpath([pwd, '/util/precomp'])
+rmpath([pwd, '/util/invToeplitz'])
